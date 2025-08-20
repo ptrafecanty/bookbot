0852185a1d76d4980812b404d835d748b94051fc
ptrafecanty <ptrafecanty@gmail.com>
2025-08-20 14:09:37 -0600

    Added stats

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from stats import get_book_text, word_counter, character_counter
+from stats import sys, get_book_text, word_counter, character_counter, sort_on, sorted_characters
 
 def main():
-    contents = get_book_text(f"books/frankenstein.txt")
-    print(contents)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filename = sys.argv[1]
+    #contents = get_book_text(f"books/frankenstein.txt")
+    contents = get_book_text(filename)
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {filename}...")
+    print("----------- Word Count ----------")
+    #print(contents)
     print(word_counter(contents))
-    print(character_counter(contents))
+    print("--------- Character Count -------")
+    characters_counted = (character_counter(contents))
+    sorted_list = sorted_characters(characters_counted)
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+
 main()
